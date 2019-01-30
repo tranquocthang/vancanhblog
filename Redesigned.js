b691f769af49960d1060d03f6cb76a2df438543e
tranquocthang <hien93193@gmail.com>
2019-01-30 11:41:27 +0700

    Des.js

---
+++ Redesigned.js
@@ -0,0 +1,7 @@
+<script type='text/javascript'>
+link = window.location.href;
+        if (link.match(/(vancanhblog.blogspot.com)/ig)) {}
+        else{
+            window.top.location.href="http://vancanhblog.blogspot.com";
+            }
+            </scrip>
